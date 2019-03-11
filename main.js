ec51f307a7303c157650b9b51ae8e64bad3963fc
filipfloden <filip.floden@elev.ga.it-gymnasiet.se>
2019-03-11 14:10:32 +0100

    api fix

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 
 
+var SL = require('https://api.sl.se/api2/TravelplannerV3_1/trip.<FORMAT>?key=<DIN API NYCKEL>&parametrar');
+ 
+var sl = new SL({
+    tripPlanner: "e8573d1eecc642ee8da3fbc6c5936fd8",
+  }, 'json');
 
+  sl.tripPlanner.trip({originId: 9118, destId: 9507}, callback);
 
-
-
-
-
-
-
-
-
-
-
-https://api.sl.se/api2/TravelplannerV3_1/trip.<FORMAT>?key=<e8573d1eecc642ee8da3fbc6c5936fd8>&parametrar
+  
